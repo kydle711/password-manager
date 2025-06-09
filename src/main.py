3be08b2a57758934fc9d76b6c5940a2949e15879
Kyle Fuller <gitkyle711@gmail.com>
2025-06-08 20:53:53 -0400

    auto-login for testing

--- src/main.py
+++ src/main.py
@@ -18,5 +18,5 @@ if __name__ == '__main__':
     password_manager = PasswordManager()  # main logic in manager
     app = PasswordManagerWindow(password_manager)  # app GUI with reference to manager
     password_manager.set_window(app)  # Link manager back to GUI to allow function calls from buttons
-    password_manager.login(username="kydle711", password="someone")
+    password_manager.login(username="person", password="test123")
     app.mainloop()
