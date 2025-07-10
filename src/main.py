62ecd5ed4c98cd4d2622eeb3aabd5034ce3ebdeb
Kyle Fuller <gitkyle711@gmail.com>
2025-07-10 17:38:09 -0400

    Remove testing line from main

--- src/main.py
+++ src/main.py
@@ -18,5 +18,4 @@ if __name__ == '__main__':
     password_manager = PasswordManager()  # main logic in manager
     app = PasswordManagerWindow(password_manager)  # app GUI with reference to manager
     password_manager.set_window(app)  # Link manager back to GUI to allow function calls from buttons
-    password_manager.login(username="person", password="test123")
     app.mainloop()
